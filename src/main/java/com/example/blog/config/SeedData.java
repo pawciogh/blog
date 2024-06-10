12a2affd2c040e2526e3ea174f823dc9fcf35779
pawciogh <pklosinski@imperosoftware.com>
2024-06-10 16:40:17 +0100

    Fake posts added to the home page

--- src/main/java/com/example/blog/config/SeedData.java
+++ src/main/java/com/example/blog/config/SeedData.java
@@ -2,10 +2,10 @@ package com.example.blog.config;
 
 import com.example.blog.models.Post;
 import com.example.blog.services.PostService;
+import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -21,11 +21,12 @@ public class SeedData implements CommandLineRunner {
         List<Post> posts = postService.getAll();
 
         if (posts.isEmpty()) { // Check if there are no existing posts
+            Faker faker = new Faker();
 
-            for (int i = 0; i < 10; i++) { // Create 5 random posts
+            for (int i = 0; i < 10; i++) { // Create 10 random posts
                 Post post = new Post();
-                post.setTitle("Post " + (i + 1)); // Generate a random title
-                post.setBody(RandomStringUtils.randomAlphabetic(100)); // Generate a random body
+                post.setTitle(faker.book().title()); // Generate a random title
+                post.setBody(faker.lorem().paragraph()); // Generate a random body
                 postService.save(post); // Save the post
             }
         }
